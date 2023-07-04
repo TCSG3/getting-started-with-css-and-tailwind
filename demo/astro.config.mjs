c0cb4f706e59564b0a033e313da835dbeb9f48e0
Kobe Ruado <insidiae423@gmail.com>
2023-07-04 15:36:57 +0800

    Add playground page

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	//? We'll start without Tailwind for the intro topics
+	// integrations: [],
 	integrations: [tailwind()],
 });
